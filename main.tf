10df2ee2bfd3996d11d3baa933652e28eb83fcb7
Ashish Verma <ashishverma.pu@gmail.com>
2022-07-17 16:38:51 -0400

    Adding vnet to make it a private cluster

--- main.tf
+++ main.tf
@@ -5,39 +5,68 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "cluster"
+# resource "azurerm_network_security_group" "example" {
+#   name                = "nsg"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+# }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "vnet"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "cluster"
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-    linux_os_config {
-      sysctl_config {
-        vm_max_map_count = 262144
-      }
-    }
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
   }
 
-  identity {
-    type = "SystemAssigned"
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
   }
 
   tags = {
-    Environment = "Production"
+    environment = "Production"
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
-  sensitive = true
-}
+# resource "azurerm_kubernetes_cluster" "example" {
+#   name                = "cluster"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   dns_prefix          = "cluster"
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 1
+#     vm_size    = "Standard_DS2_v2"
+#     # linux_os_config {
+#     #   sysctl_config {
+#     #     vm_max_map_count = 2621440
+#     #   }
+#     # }
+#   }
 
-  sensitive = true
-}
+#   identity {
+#     type = "SystemAssigned"
+#   }
+
+#   tags = {
+#     Environment = "Production"
+#   }
+# }
+
+# output "client_certificate" {
+#   value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+#   sensitive = true
+# }
+
+# output "kube_config" {
+#   value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+#   sensitive = true
+# }
