6598d5e99d8734730db5412c5cecc31af7281d39
Ashish Verma <ashishverma.pu@gmail.com>
2022-07-31 15:32:13 -0400

    Trying azure cni

--- main.tf
+++ main.tf
@@ -19,18 +19,18 @@ resource "azurerm_virtual_network" "example" {
 
 resource "azurerm_subnet" "kubernetes" {
   name = "kubernetes"
-  address_prefixes = [ "10.0.2.0/24" ]
+  address_prefixes = [ "10.0.0.0/16" ]
   resource_group_name = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = "ashisverma"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Premium"
-  admin_enabled       = false
-}
+# resource "azurerm_container_registry" "acr" {
+#   name                = "ashisverma"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   sku                 = "Premium"
+#   admin_enabled       = false
+# }
 
 resource "azurerm_kubernetes_cluster" "example" {
   name                = "cluster"
@@ -46,10 +46,10 @@ resource "azurerm_kubernetes_cluster" "example" {
   }
 
   network_profile {
-    network_plugin = "kubenet"
-    service_cidr = "172.16.0.0/16"
-    dns_service_ip = "172.16.0.10"
-    docker_bridge_cidr = "172.17.0.1/16"
+    network_plugin = "azure"
+    service_cidr = "10.100.0.0/16"
+    dns_service_ip = "10.100.0.10"
+    docker_bridge_cidr = "10.200.0.0/16"
   }
 
   identity {
@@ -61,9 +61,9 @@ resource "azurerm_kubernetes_cluster" "example" {
   }
 }
 
-resource "azurerm_role_assignment" "example" {
-  principal_id                     = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-}
+# resource "azurerm_role_assignment" "example" {
+#   principal_id                     = azurerm_kubernetes_cluster.example.kubelet_identity[0].object_id
+#   role_definition_name             = "AcrPull"
+#   scope                            = azurerm_container_registry.acr.id
+#   skip_service_principal_aad_check = true
+# }
