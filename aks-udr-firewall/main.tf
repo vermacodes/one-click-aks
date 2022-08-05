866ff8b9d49181de47eaccc621f37e919e54f1a4
Ashish Verma <ashishverma.pu@gmail.com>
2022-08-05 17:31:39 -0400

    Added standard lb cluster

--- aks-udr-firewall/main.tf
+++ aks-udr-firewall/main.tf
@@ -130,7 +130,8 @@ module "virtual_network" {
       routes = {
         internet = {
           address_prefix = "0.0.0.0/0"
-          next_hop_type  = "Internet"
+          next_hop_type  = "VirtualAppliance"
+          next_hop_in_ip_address = "10.1.2.4"
         }
         local-vnet = {
           address_prefix = "10.1.0.0/22"
@@ -142,11 +143,193 @@ module "virtual_network" {
 }
 
 module "firewall" {
-  source              = "github.com/vermacodes/terraform-azurerm-firewall.git?ref=main"
+  #source              = "github.com/vermacodes/terraform-azurerm-firewall.git?ref=main"
+  source              = "../../azure-terraform/terraform-azurerm-firewall/"
   location            = module.metadata.location
   names               = module.metadata.names
   tags                = module.metadata.tags
   resource_group_name = module.resource_group.name
+  firewall_subnet_id  = module.virtual_network.subnets["AzureFirewallSubnet"].id
+
+  firewall_network_rules = {
+    "network_rule1" = {
+      name         = "firewall-network-rule-aks"
+      action       = "Allow"
+      priority     = "100"
+      rules = [{
+        name                  = "Azure Workload AKS Subnets - Ubuntu"
+        description           = "Azure Workload AKS Subnets - Ubuntu"
+        source_addresses      = ["10.1.0.0/24", "10.1.1.0/24"]
+        destination_ports     = ["123"]
+        destination_addresses = ["91.189.89.199", "91.189.89.198", "91.189.94.4", "91.189.91.157"]
+        protocols             = ["UDP"]
+        }]
+    }
+  }
+
+  firewall_application_rules = {
+    "app_rule1" = {
+      name         = "app-rule-aks"
+      priority     = "200"
+      action       = "Allow"
+      rules = [
+        {
+          name             = "Azure Global required FQDN / application rules"
+          description      = "Azure Global required FQDN / application rules"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null #["Azurebackup"]
+          target_fqdns     = ["*.cdn.mscr.io", "*.hcp.eastus.azmk8s.io", "management.azure.com", "login.microsoftonline.com", "acs-mirror.azureedge.net", "packages.microsoft.com", "*.data.mcr.microsoft.com", "mcr.microsoft.com"]
+          protocol = [{
+            port = 443
+            type = "Https"
+
+          }]
+        },
+        {
+          name             = "Azure US Government required FQDN / application rules"
+          description      = "Azure US Government required FQDN / application rules"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["*.cdn.mscr.io", "management.usgovcloudapi.net", "login.microsoftonline.com", "acs-mirror.azureedge.net", "*.hcp.eastus.cx.aks.containerservice.azure.us", "packages.microsoft.com", "*.data.mcr.microsoft.com", "mcr.microsoft.com"]
+          protocol = [{
+            port = 443
+            type = "Https"
+
+          }]
+        },
+        {
+          name             = "Optional recommended FQDN / application rules for AKS clusters"
+          description      = "Optional recommended FQDN / application rules for AKS clusters"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["security.ubuntu.com", "azure.archive.ubuntu.com", "changelogs.ubuntu.com"]
+          protocol = [{
+            port = 80
+            type = "Http"
+
+          }]
+        },
+        {
+          name             = "GPU enabled AKS clusters"
+          description      = "GPU enabled AKS clusters"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["nvidia.github.io", "us.download.nvidia.com", "apt.dockerproject.org"]
+          protocol = [{
+            port = 443
+            type = "Https"
+
+          }]
+        },
+        {
+          name             = "Azure Workload AKS Subnet - Azure Monitoring For Containier"
+          description      = "Azure Workload AKS Subnet - Azure Monitoring For Containier"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["*.ods.opinsights.azure.com", "dc.services.visualstudio.com", "*.oms.opinsights.azure.com", "*.monitoring.azure.com", "*.applicationinsights.azure.com", "*.cloudapp.net", "*.monitor.azure.com", "*.trafficmanager.net", "rt.services.visualstudio.com", "*.blob.core.windows.net", "*.azure-automation.net", "*.loganalytics.io", "*.applicationinsights.io", "169.254.169.254"]
+          protocol = [{
+            port = 443
+            type = "Https"
+
+          }]
+        },
+        {
+          name             = "Azure Workload AKS Subnet - Azure Dev Spaces"
+          description      = "Azure Workload AKS Subnet - Azure Dev Spaces"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["gcr.io", "storage.googleapis.com", "cloudflare.docker.com"]
+          protocol = [{
+            port = 443
+            type = "Https"
+
+          }]
+        },
+        {
+          name             = "Azure Workload AKS Subnet - Docker Repository"
+          description      = "Azure Workload AKS Subnet - Docker Repository"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["*.docker.io", "*.gcr.io", "*.docker.com", "gcr.io", "*.googleapis.com"]
+          protocol = [{
+            port = 443
+            type = "Https"
+
+          }]
+        },
+        {
+          name             = "Azure Workload AKS Subnet - Azurepolicy"
+          description      = "Azure Workload AKS Subnet - Azurepolicy"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["dc.services.visualstudio.com", "raw.githubusercontent.com", "gov-prod-policy-data.trafficmanager.net"]
+          protocol = [{
+            port = 443
+            type = "Https"
+
+          }]
+        },
+        {
+          name             = "AzureKubernetesService"
+          description      = "AzureKubernetesService"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["AzureKubernetesService"]
+          protocol = [{
+            port = 443
+            type = "Https"
+
+          }]
+        }
+      ]
+    },
+    "app_rule3" = {
+      name         = "firewall-app-rule-linkerd"
+      priority     = "300"
+      action       = "Allow"
+      rules = [
+        {
+          name             = "LinkerD"
+          description      = "LinderD to pull images"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["cr.l5d.io", "ghcr.io", "*.githubusercontent.com", "*.linkerd.io", "*.amazonaws.com"]
+          protocol = [{
+            port = 443
+            type = "Https"
+            },
+            {
+              port = 80
+              type = "Http"
+            }
+          ]
+        }
+      ]
+    }
+    "app_rule4" = {
+      name         = "firewall-app-rule-nodered"
+      priority     = "400"
+      action       = "Allow"
+      rules = [
+        {
+          name             = "NodeRed"
+          description      = "NodeRed to pull images"
+          source_addresses = ["10.1.0.0/24", "10.1.1.0/24"]
+          fqdn_tags        = null
+          target_fqdns     = ["registry.npmjs.org"]
+          protocol = [{
+            port = 443
+            type = "Https"
+            },
+            {
+              port = 80
+              type = "Http"
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
 
 module "kubernetes" {
@@ -167,6 +350,11 @@ module "kubernetes" {
   network_plugin = "azure"
 
   configure_network_role = true
+
+  outbound_type = "userDefinedRouting"
+
+  # Enable/Disable line below to enable/disable authorized ip ranges.
+  api_server_authorized_ip_ranges = {"my_ip" = "${chomp(data.http.my_ip.response_body)}/32"}
 
   virtual_network = {
     subnets = {
