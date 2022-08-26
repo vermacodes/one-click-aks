6c091c43ae9f49d5abab9bee30d3d01c085c2f00
Ashish Verma <ashishverma.pu@gmail.com>
2022-08-26 18:16:15 -0400

    Add TODO

--- aks-standard-lb-2/destroy.sh
+++ aks-standard-lb-2/destroy.sh
@@ -7,5 +7,9 @@ if [ $? -ne 0 ]; then
     echo "Terraform is not installed. Installation Instructions -> https://learn.hashicorp.com/tutorials/terraform/install-cli"
 fi
 
+# TODO: Add a section to pull credentials of this exact cluster based on the state
+# thi is needed to delete in nginx ingress deployment before other resources could be destroyed.
+# not sure if there is a better way.
+
 terraform init
 terraform destroy -auto-approve
