82f3a20663e3bb8bb40889cd1deea57d8e26b5b1
Ashish Verma <ashishverma.pu@gmail.com>
2022-09-09 11:53:30 -0400

    Add global scripts

---
+++ apply.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Parameter input validation
+if [ -z "$1" ]
+then
+    echo "Directory name required. run like, /bin/bash apply.sh <directory-name>"
+    exit 1
+fi
+
+cd $1
+
+# Validate if terraform in installed
+dpkg -s terraform &> /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Terraform is not installed. Installation Instructions -> https://learn.hashicorp.com/tutorials/terraform/install-cli"
+fi
+
+terraform init
+terraform apply -auto-approve
