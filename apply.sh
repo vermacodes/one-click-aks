a11b04976c3f71710af8b80cb92c2dea08724f18
Ashish Verma <ashishverma.pu@gmail.com>
2022-09-09 12:22:28 -0400

    Add directory check

--- apply.sh
+++ apply.sh
@@ -2,13 +2,16 @@
 
 
 # Parameter input validation
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     echo "Directory name required. run like, /bin/bash apply.sh <directory-name>"
     exit 1
 fi
 
 cd $1
+if [ $? -ne 0 ]; then
+    echo "Looks like wrong directory name"
+    exit 1
+fi
 
 # Validate if terraform in installed
 dpkg -s terraform &> /dev/null
