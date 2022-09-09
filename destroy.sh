cc51b4fca5cabac05215d447582309455959183d
Ashish Verma <ashishverma.pu@gmail.com>
2022-09-09 11:53:37 -0400

    Add global script

---
+++ destroy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Parameter input validation
+if [ -z "$1" ]
+then
+    echo "Directory name required. run like, /bin/bash apply.sh <directory-name>"
+    exit 1
+fi
+
+cd $1
+
+# Validate if terraform in installed
+dpkg -s terraform &> /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Terraform is not installed. Installation Instructions -> https://learn.hashicorp.com/tutorials/terraform/install-cli"
+fi
+
+# TODO: Add a section to pull credentials of this exact cluster based on the state
+# thi is needed to delete in nginx ingress deployment before other resources could be destroyed.
+# not sure if there is a better way.
+
+terraform init
+terraform destroy -auto-approve
