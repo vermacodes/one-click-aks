e11f8975bc789be7cd3593050825bbeaba36143a
Ashish Verma <ashishverma.pu@gmail.com>
2022-11-07 12:02:54 -0500

    Add CORS

--- app/server/main.go
+++ app/server/main.go
@@ -22,7 +22,7 @@ func status(c *gin.Context) {
 func handleRequests() {
 	router := gin.Default()
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowOrigins = []string{"http://localhost:3000", "https://ashisverma.z13.web.core.windows.net"}
 	router.Use(cors.New(config))
 
 	router.POST("/apply", apply)
