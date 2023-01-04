22b22d639ab54dbd960cae2449e29755f00f8f71
vermacodes <ashishverma.pu@gmail.com>
2023-01-04 17:02:14 -0500

    Output only if resource is deployed.

--- tf/log_analytics_workspace_output.tf
+++ tf/log_analytics_workspace_output.tf
@@ -1,9 +1,9 @@
 output "log_analytics_workspace_resource_id" {
-  value       = azurerm_log_analytics_workspace.this[0].id
+  value       = var.kubernetes_cluster.addons.microsoft_defender ? azurerm_log_analytics_workspace.this[0].id : ""
   description = "The Log Analytics Workspace ID."
 }
 
 output "log_analytics_workspace_id" {
-  value       = azurerm_log_analytics_workspace.this[0].workspace_id
+  value       = var.kubernetes_cluster.addons.microsoft_defender ? azurerm_log_analytics_workspace.this[0].workspace_id : ""
   description = "The Workspace (or Customer) ID for the Log Analytics Workspace."
 }
