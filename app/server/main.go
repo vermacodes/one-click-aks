0efd28ebd555648ce18c9be5d29b8a4d0d42d72b
vermacodes <ashishverma.pu@gmail.com>
2023-01-26 14:00:27 -0500

    Logo

--- app/server/main.go
+++ app/server/main.go
@@ -45,7 +45,7 @@ func main() {
 	router.SetTrustedProxies(nil)
 
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost:5173", "https://ashisverma.z13.web.core.windows.net", "https://*.azurewebsites.net"}
+	config.AllowOrigins = []string{"http://localhost:3000", "http://localhost:5173", "https://ashisverma.z13.web.core.windows.net", "https://actlabs.z13.web.core.windows.net", "https://*.azurewebsites.net"}
 
 	router.Use(cors.New(config))
 
