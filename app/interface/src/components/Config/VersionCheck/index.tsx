f8c5d159a6e1d2f97ff5d7878913bd463eaac903
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-10-17 19:23:40 -0400

    dont show message when fetching the deployments.

--- app/interface/src/components/Config/VersionCheck/index.tsx
+++ app/interface/src/components/Config/VersionCheck/index.tsx
@@ -1,23 +1,22 @@
 import { useGetMyDeployments } from "../../../hooks/useDeployments";
 
 export default function VersionCheck() {
-  const { data: deployments, isError } = useGetMyDeployments();
+  const { data: deployments, isFetching } = useGetMyDeployments();
   return (
     <div className="my-4">
-      {isError ||
-        (!deployments && (
-          <div className="mt-2 rounded border border-red-500 bg-red-500 bg-opacity-20 p-2">
-            <strong>Oh no!</strong> There is a version mismatch between server
-            and client. That means its time to upgrade your server.{" "}
-            <a
-              href="https://actlabsdocs.z13.web.core.windows.net/docs/getting-started"
-              className="cursor-pointer hover:text-sky-500 hover:underline"
-              target="_blank"
-            >
-              Click here for instructions.
-            </a>
-          </div>
-        ))}
+      {!isFetching && !deployments && (
+        <div className="mt-2 rounded border border-red-500 bg-red-500 bg-opacity-20 p-2">
+          <strong>Oh no!</strong> There is a version mismatch between server and
+          client. That means its time to upgrade your server.{" "}
+          <a
+            href="https://actlabsdocs.z13.web.core.windows.net/docs/getting-started"
+            className="cursor-pointer hover:text-sky-500 hover:underline"
+            target="_blank"
+          >
+            Click here for instructions.
+          </a>
+        </div>
+      )}
     </div>
   );
 }
