7242a0c5334486594a328911b80330dab88a94fc
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-02 15:58:14 -0400

    add builder container

--- app/interface/src/components/Builder/KubernetesCluster/KubernetesCluster/index.tsx
+++ app/interface/src/components/Builder/KubernetesCluster/KubernetesCluster/index.tsx
@@ -1,10 +1,5 @@
-import { useContext, useState } from "react";
-import { TfvarKubernetesClusterType } from "../../../../dataStructures";
-import { useLab, useSetLab } from "../../../../hooks/useLab";
-import { useSetLogs } from "../../../../hooks/useLogs";
-import { useGetOrchestrators } from "../../../../hooks/useOrchestrators";
-import { defaultKubernetesCluster } from "../../../../defaults";
-import { WebSocketContext } from "../../../../WebSocketContext";
+import { useState } from "react";
+import { useLab } from "../../../../hooks/useLab";
 import Version from "../Version";
 import PrivateCluster from "../PrivateCluster";
 import VirtualMachine from "../../VirtualMachine";
@@ -17,44 +12,11 @@ import AppGateway from "../Addons/AppGateway";
 import MicrosoftDefender from "../Addons/MicrosoftDefender";
 import VirtualNode from "../Addons/VirtualNode";
 import HttpApplicationRouting from "../Addons/HttpApplicationRouting";
+import BuilderContainer from "../../../UserInterfaceComponents/BuilderContainer";
 
 export default function KubernetesCluster() {
   const [versionMenu, setVersionMenu] = useState<boolean>(false);
   const { data: lab } = useLab();
-  const { mutate: setLab } = useSetLab();
-  const { actionStatus } = useContext(WebSocketContext);
-  const { mutate: setLogs } = useSetLogs();
-
-  const { data: kubernetesVersion } = useGetOrchestrators();
-
-  // The default value that kubernetes cluster will carry.
-  function defaultValue(): TfvarKubernetesClusterType {
-    var defaultVersion = "";
-    if (kubernetesVersion && kubernetesVersion.values) {
-      defaultVersion = Object.keys(
-        kubernetesVersion.values[0].patchVersions
-      )[0];
-    }
-
-    return { ...defaultKubernetesCluster, kubernetesVersion: defaultVersion };
-  }
-
-  function handleOnChange() {
-    if (lab !== undefined) {
-      if (lab.template !== undefined) {
-        if (lab.template.kubernetesClusters.length === 0) {
-          lab.template.kubernetesClusters = [defaultValue()];
-        } else {
-          lab.template.kubernetesClusters = [];
-        }
-        !actionStatus.inProgress &&
-          setLogs({
-            logs: JSON.stringify(lab.template, null, 4),
-          });
-        setLab(lab);
-      }
-    }
-  }
 
   if (
     lab === undefined ||
@@ -66,14 +28,8 @@ export default function KubernetesCluster() {
   }
 
   return (
-    <div
-      className={`mt-4 flex w-full flex-col gap-x-2 gap-y-2 rounded border p-4 shadow dark:border-slate-800 dark:shadow-slate-800`}
-    >
-      <p className="text-lg font-bold">Kubernetes Cluster </p>
-      <div
-        className={`mt-4 flex w-full flex-col gap-x-2 gap-y-2 rounded border p-4 shadow dark:border-slate-800 dark:shadow-slate-800`}
-      >
-        <p className="text-lg font-bold">Features </p>
+    <BuilderContainer title="Kubernetes Cluster">
+      <BuilderContainer title="Features">
         <div className={`mt-4 flex flex-wrap gap-x-2 gap-y-2`}>
           <Version versionMenu={versionMenu} setVersionMenu={setVersionMenu} />
           <PrivateCluster />
@@ -84,18 +40,15 @@ export default function KubernetesCluster() {
           <AutoScaling />
           <UserDefinedRouting />
         </div>
-      </div>
-      <div
-        className={`mt-4 flex w-full flex-col gap-x-2 gap-y-2 rounded border p-4 shadow dark:border-slate-800 dark:shadow-slate-800`}
-      >
-        <p className="text-lg font-bold">Addons </p>
+      </BuilderContainer>
+      <BuilderContainer title="Addons">
         <div className={`mt-4 flex flex-wrap gap-x-2 gap-y-2`}>
           <AppGateway />
           <MicrosoftDefender />
           <VirtualNode />
           <HttpApplicationRouting />
         </div>
-      </div>
-    </div>
+      </BuilderContainer>
+    </BuilderContainer>
   );
 }
