12cad6425939018ead86d6fe580c66f2f13f12fa
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-02 02:21:24 -0400

    Feedback Icon

--- app/interface/src/components/Navbar/index.tsx
+++ app/interface/src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import {
   FaClipboard,
   FaCog,
+  FaComments,
   FaExternalLinkAlt,
   FaFileCode,
   FaFlask,
@@ -199,9 +200,9 @@ function FixedPages({ darkMode, setDarkMode }: FixedPagesProps) {
         </li>
         <li>
           <a target="_blank" href="https://forms.office.com/r/R513F2YfXE">
-            <button className="flex h-full w-full items-center justify-start gap-2 rounded py-3 px-4 text-left text-base hover:bg-slate-200 dark:hover:bg-slate-800">
+            <button className="flex h-full w-full items-center justify-start gap-2 rounded py-3 px-4 text-left text-lg hover:bg-slate-200 dark:hover:bg-slate-800">
               <span>
-                <FaExternalLinkAlt />
+                <FaComments />
               </span>
               <span className="font-bold text-green-500">Share Feedback</span>
             </button>
