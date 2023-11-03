6846124099db55a4c37f675448303bdee31f1822
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-03 12:57:41 -0400

    bg opacity when in light mode

--- app/interface/src/components/Deployments/CreateNewDeployment/index.tsx
+++ app/interface/src/components/Deployments/CreateNewDeployment/index.tsx
@@ -81,14 +81,14 @@ function Modal({ showModal, setShowModal }: ModalProps) {
   if (!showModal) return null;
   return (
     <div
-      className="fixed inset-0 z-20 flex max-h-full max-w-full justify-center bg-slate-800 dark:bg-slate-100 dark:bg-opacity-80"
+      className="fixed inset-0 z-20 flex max-h-full max-w-full justify-center bg-slate-800 bg-opacity-80 dark:bg-slate-100 dark:bg-opacity-80"
       onClick={(e) => {
         setShowModal(false);
         e.stopPropagation();
       }}
     >
       <div
-        className="my-20 h-1/3 w-1/3 space-y-2 divide-y divide-slate-300 overflow-y-auto overflow-x-hidden rounded bg-slate-100 p-5 scrollbar-thin  scrollbar-thumb-slate-400 dark:divide-slate-700 dark:bg-slate-900 dark:scrollbar-thumb-slate-600"
+        className="my-20 h-1/3 w-1/3 space-y-2 divide-y divide-slate-300 overflow-y-auto rounded bg-slate-100 p-5 overflow-x-hidden scrollbar-thin  scrollbar-thumb-slate-400 dark:divide-slate-700 dark:bg-slate-900 dark:scrollbar-thumb-slate-600"
         onClick={(e) => {
           e.stopPropagation();
         }}
