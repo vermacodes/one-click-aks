917cf0c0faa0b40933ad646f30d858e22c773fce
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-06 17:37:26 -0500

    better messaging

--- app/interface/src/components/Config/WebSocketConnectionStatus/index.tsx
+++ app/interface/src/components/Config/WebSocketConnectionStatus/index.tsx
@@ -12,17 +12,28 @@ export default function WebSocketConnectionStatus() {
   return (
     <div className="my-4">
       <div className="mt-2 rounded border border-red-500 bg-red-500 bg-opacity-20 p-2">
-        <strong>Oh no!</strong> Websocket connection seems to be in trouble. If
-        this persists, goto{" "}
-        <a
-          href="/settings"
-          className="cursor-pointer hover:text-sky-500 hover:underline"
-          target="_blank"
-        >
-          Settings
-        </a>
-        , and reset the `Server Endpoint` or `Reset Server Cache` from the
-        bottom of the page.
+        <strong>Connection Issue Detected:</strong> It appears there may be a
+        problem with the websocket connection.
+        <br />
+        To fix, please follow these steps:
+        <ol className="ml-4 list-decimal">
+          <li>
+            Navigate to{" "}
+            <a
+              href="/settings"
+              className="cursor-pointer text-sky-500 underline"
+              target="_blank"
+            >
+              Settings
+            </a>{" "}
+            and re-select the selected 'Server Endpoint'. (Click on the already
+            checked switch)
+          </li>
+          <li>
+            If you still see this message, please contact for support. This is a
+            known issue and we are still trying to understand it fully.
+          </li>
+        </ol>
       </div>
     </div>
   );
