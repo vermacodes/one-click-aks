d3ed947421c4f5740b40b145ec516d54993706c9
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-05 22:17:50 -0500

    click on header to close and open

--- app/interface/src/components/UserInterfaceComponents/BuilderContainer/index.tsx
+++ app/interface/src/components/UserInterfaceComponents/BuilderContainer/index.tsx
@@ -13,7 +13,10 @@ export default function BuilderContainer({ title, children }: Props) {
     <div
       className={`my-4 flex w-full flex-col rounded border p-2 shadow-sm dark:border-slate-700 dark:shadow-slate-700`}
     >
-      <div className="flex items-center justify-between">
+      <div
+        className="flex items-center justify-between"
+        onClick={() => setOpen(!open)}
+      >
         <p className="text-lg font-bold">{title}</p>
         <div className={`${!open && "rotate-180"} transition-all duration-500`}>
           <Button variant="secondary-icon" onClick={() => setOpen(!open)}>
