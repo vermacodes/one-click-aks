e26da5de5881710049de25cb6170785e5887119c
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-10 14:08:39 -0500

    better messaging

--- app/interface/src/components/Config/WebSocketConnectionStatus/index.tsx
+++ app/interface/src/components/Config/WebSocketConnectionStatus/index.tsx
@@ -10,31 +10,17 @@ export default function WebSocketConnectionStatus() {
   }
 
   return (
-    <div className="my-4">
-      <div className="mt-2 rounded border border-red-500 bg-red-500 bg-opacity-20 p-2">
-        <strong>Connection Issue Detected:</strong> It appears there may be a
-        problem with the websocket connection.
-        <br />
-        To fix, please follow these steps:
-        <ol className="ml-4 list-decimal">
-          <li>
-            Navigate to{" "}
-            <a
-              href="/settings"
-              className="cursor-pointer text-sky-500 underline"
-              target="_blank"
-            >
-              Settings
-            </a>{" "}
-            and re-select the selected 'Server Endpoint'. (Click on the already
-            checked switch)
-          </li>
-          <li>
-            If you still see this message, please contact for support. This is a
-            known issue and we are still trying to understand it fully.
-          </li>
-        </ol>
-      </div>
+    <div className="z-5 mt-2 rounded border border-yellow-500 bg-yellow-500 bg-opacity-20 p-2">
+      <strong>⚠️ Connection Issue Detected:</strong> There seems to be a problem
+      with the websocket connection. This gets fixed if you{" "}
+      <a
+        href="#"
+        onClick={() => window.location.reload()}
+        className="cursor-pointer text-sky-600 underline"
+      >
+        Refresh
+      </a>{" "}
+      browser. Use Help & Feedback if the problem continues.
     </div>
   );
 }
