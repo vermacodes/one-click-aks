c49c6dc35b091bed66ad80edda5ec995c7cda0e5
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-13 17:38:28 -0500

    no need change directory

--- app/interface/publishui.sh
+++ app/interface/publishui.sh
@@ -9,8 +9,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-cd app/interface
-
 if [[ "$1" == "" ]]; then
     sa="actlabs"
 else
