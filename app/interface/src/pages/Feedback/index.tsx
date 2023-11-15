93ff624792a67d0024e1fff19322c413ba5e0255
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-15 00:12:46 -0500

    style

--- app/interface/src/pages/Feedback/index.tsx
+++ app/interface/src/pages/Feedback/index.tsx
@@ -4,7 +4,7 @@ export default function Feedback() {
   return (
     <PageLayout>
       <iframe
-        className="h-screen w-full max-w-full overflow-hidden py-4 "
+        className="h-screen w-full max-w-full overflow-hidden py-4"
         src="https://forms.office.com/Pages/ResponsePage.aspx?id=v4j5cvGGr0GRqy180BHbRzY0DairMyJDrbQ9kViEZ25UMzdFWExRM0NZR1lUTlJRSFhIQUc0V1RIMi4u&embed=true"
       />
     </PageLayout>
