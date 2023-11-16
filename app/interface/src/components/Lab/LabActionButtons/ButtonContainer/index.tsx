36d03531a8205e04b252c77844b94ea71738ac49
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-16 02:04:11 -0500

    dynamic button

---
+++ app/interface/src/components/Lab/LabActionButtons/ButtonContainer/index.tsx
@@ -0,0 +1,129 @@
+import {
+  useState,
+  useEffect,
+  useRef,
+  useLayoutEffect,
+  useCallback,
+} from "react";
+import Button from "../../../UserInterfaceComponents/Button";
+import { FaEllipsisV } from "react-icons/fa";
+import { ButtonContainerObj } from "../../../../dataStructures";
+
+type Props = {
+  buttons: Record<string, ButtonContainerObj>;
+  setButtons(args: Record<string, ButtonContainerObj>): void;
+  overflowButtons: Record<string, ButtonContainerObj>;
+  setOverflowButtons(args: Record<string, ButtonContainerObj>): void;
+};
+
+export default function ButtonContainer({
+  buttons,
+  setButtons,
+  overflowButtons,
+  setOverflowButtons,
+}: Props) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const hiddenButtonRef = useRef<HTMLButtonElement>(null);
+  const [buttonWidth, setButtonWidth] = useState(0);
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
+
+  const handleMouseEnterDropdown = useCallback(() => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+  }, [timeoutId]);
+
+  const onDropdownOpen = useCallback(() => {
+    setShowDropdown(!showDropdown);
+    const id = setTimeout(() => {
+      setShowDropdown(false);
+    }, 2000);
+    setTimeoutId(id);
+  }, []);
+
+  useLayoutEffect(() => {
+    if (hiddenButtonRef.current) {
+      setButtonWidth(hiddenButtonRef.current.offsetWidth);
+    }
+  }, []);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const resizeObserver = new ResizeObserver(() => {
+      const containerWidth = container?.offsetWidth;
+      const allButtons = { ...overflowButtons, ...buttons };
+      const buttonsArray = Object.entries(allButtons);
+      buttonsArray.sort((a, b) => a[1].order - b[1].order);
+      const sortedButtons = Object.fromEntries(buttonsArray);
+
+      if (containerWidth && buttonWidth) {
+        const maxButtons = Math.floor(containerWidth / buttonWidth);
+        if (
+          Object.keys(sortedButtons).length < maxButtons &&
+          Object.keys(overflowButtons).length > 0
+        ) {
+          setButtons({ ...sortedButtons });
+          setOverflowButtons({});
+        }
+        if (
+          Object.keys(sortedButtons).length > maxButtons &&
+          Object.keys(buttons).length !== maxButtons
+        ) {
+          setButtons(
+            Object.fromEntries(
+              Object.entries(sortedButtons).slice(0, maxButtons)
+            )
+          );
+
+          setOverflowButtons(
+            Object.fromEntries(Object.entries(sortedButtons).slice(maxButtons))
+          );
+        }
+      }
+    });
+
+    if (container) {
+      resizeObserver.observe(container);
+    }
+
+    return () => {
+      if (container) {
+        resizeObserver.unobserve(container);
+      }
+    };
+  }, [buttonWidth, buttons, overflowButtons, containerRef]);
+
+  return (
+    <div className="flex w-full" ref={containerRef}>
+      <div className="flex w-full justify-between">
+        <div className="flex w-full space-x-4">
+          {Object.values(buttons).map((button) => button.button)}
+        </div>
+        {Object.values(overflowButtons).length > 0 && (
+          <div className="relative inline-block text-left">
+            <Button onClick={onDropdownOpen} variant="secondary-icon">
+              <FaEllipsisV />
+            </Button>
+            {showDropdown && (
+              <div
+                className="space-2 absolute right-0 mt-2 flex w-fit flex-col gap-2 whitespace-nowrap border bg-white p-4 shadow-lg"
+                onMouseLeave={() => setShowDropdown(false)}
+                onMouseEnter={handleMouseEnterDropdown}
+              >
+                {Object.values(overflowButtons).map((button) => button.button)}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <button
+        ref={hiddenButtonRef}
+        className="absolute -left-full  bg-blue-500 px-4 py-2"
+      >
+        This is a button
+      </button>
+    </div>
+  );
+}
--- app/interface/src/components/Lab/LabActionButtons/PublicLabActionButtons/index.tsx
+++ app/interface/src/components/Lab/LabActionButtons/PublicLabActionButtons/index.tsx
@@ -1,22 +1,118 @@
-import { FaShare, FaTrash } from "react-icons/fa";
-import { Lab } from "../../../../dataStructures";
+import { FaShare } from "react-icons/fa";
+import { ButtonContainerObj, Lab } from "../../../../dataStructures";
 import Button from "../../../UserInterfaceComponents/Button";
 import ExportLabButton from "../../Export/ExportLabButton";
 import LoadToBuilderButton from "../../LoadToBuilderButton";
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import DeleteLabButton from "../../DeleteLabButton";
 import { useGetMyRoles } from "../../../../hooks/useAuth";
+import ButtonContainer from "../ButtonContainer";
 
 type Props = {
   lab: Lab;
 };
 
 export default function PublicLabActionButtons({ lab }: Props) {
-  const [copied, setCopied] = useState(false);
   const { data: roles } = useGetMyRoles();
+  const [buttons, setButtons] = useState<Record<string, ButtonContainerObj>>(
+    {}
+  );
+  const [overflowButtons, setOverflowButtons] = useState<
+    Record<string, ButtonContainerObj>
+  >({});
 
-  function copyLinkToLab(lab: Lab) {
+  useEffect(() => {
+    const initialButtons: Record<string, ButtonContainerObj> = {
+      loadToBuilderButton: {
+        id: "loadToBuilderButton",
+        order: 1,
+        button: (
+          <LoadToBuilderButton
+            key={"loadToBuilderButton"}
+            lab={lab}
+            variant="primary"
+          >
+            Open
+          </LoadToBuilderButton>
+        ),
+      },
+      exportLabButton: {
+        id: "exportLabButton",
+        order: 2,
+        button: (
+          <ExportLabButton
+            key={"exportLabButton"}
+            lab={lab}
+            variant="secondary-text"
+          >
+            Export
+          </ExportLabButton>
+        ),
+      },
+      shareLabButton: {
+        id: "shareLabButton",
+        order: 3,
+        button: (
+          <Button variant="secondary-text" onClick={() => copyLinkToLab(lab)}>
+            <span>
+              <FaShare />
+            </span>
+            Share
+          </Button>
+        ),
+      },
+    };
+
+    setButtons(initialButtons);
+  }, [lab]);
+
+  function upsertButton(button: ButtonContainerObj) {
+    if (overflowButtons[button.id]) {
+      console.log("overflow button", overflowButtons);
+      setOverflowButtons((prevOverflowButtons) => {
+        return { ...prevOverflowButtons, [button.id]: button };
+      });
+      return;
+    }
+
+    setButtons((prevButtons) => {
+      return { ...prevButtons, [button.id]: button };
+    });
+
+    console.log("buttons", buttons);
+  }
+
+  function deleteButton(buttonId: string) {
+    setButtons((prevButtons) => {
+      const { [buttonId]: _, ...remainingButtons } = prevButtons;
+      return remainingButtons;
+    });
+  }
+
+  useEffect(() => {
+    if (roles?.roles.includes("admin")) {
+      const deleteButton: ButtonContainerObj = {
+        id: "deleteLabButton",
+        order: 4,
+        button: (
+          <DeleteLabButton
+            lab={lab}
+            key={"deleteLabButton"}
+            variant="danger-text"
+          >
+            Delete
+          </DeleteLabButton>
+        ),
+      };
+
+      upsertButton(deleteButton);
+    } else {
+      deleteButton("deleteLabButton");
+    }
+  }, [roles, lab]);
+
+  const copyLinkToLab = useCallback((lab: Lab) => {
     navigator.clipboard.writeText(
       `${window.location.origin}/lab/sharedtemplate/${lab.id}`
     );
@@ -26,35 +122,46 @@ export default function PublicLabActionButtons({ lab }: Props) {
       toastId: "copy-link-to-lab",
     });
 
-    // Set copied to true for 2 seconds
-    setCopied(true);
+    const shareLabButtonSuccess: ButtonContainerObj = {
+      id: "shareLabButton",
+      order: 3,
+      button: (
+        <Button variant="success" onClick={() => copyLinkToLab(lab)}>
+          <span>
+            <FaShare />
+          </span>
+          Copied
+        </Button>
+      ),
+    };
+
+    const shareLabButton: ButtonContainerObj = {
+      id: "shareLabButton",
+      order: 3,
+      button: (
+        <Button variant="secondary-text" onClick={() => copyLinkToLab(lab)}>
+          <span>
+            <FaShare />
+          </span>
+          Share
+        </Button>
+      ),
+    };
+
+    upsertButton(shareLabButtonSuccess);
     setTimeout(() => {
-      setCopied(false);
+      upsertButton(shareLabButton);
     }, 2000);
-  }
+  }, []);
 
   return (
-    <div className="flex justify-start gap-2">
-      <LoadToBuilderButton lab={lab} variant="primary">
-        Open
-      </LoadToBuilderButton>
-      <ExportLabButton lab={lab} variant="secondary-text">
-        Export
-      </ExportLabButton>
-      <Button
-        variant={`${copied ? "success-text" : "secondary-text"}`}
-        onClick={() => copyLinkToLab(lab)}
-      >
-        <span>
-          <FaShare />
-        </span>
-        {copied ? "Done" : "Share"}
-      </Button>
-      {roles?.roles.includes("admin") && (
-        <DeleteLabButton lab={lab} variant="danger-text">
-          Delete
-        </DeleteLabButton>
-      )}
+    <div className="flex flex-wrap justify-start gap-2">
+      <ButtonContainer
+        buttons={buttons}
+        setButtons={setButtons}
+        overflowButtons={overflowButtons}
+        setOverflowButtons={setOverflowButtons}
+      />
     </div>
   );
 }
