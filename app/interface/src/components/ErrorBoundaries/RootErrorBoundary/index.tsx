8ba0eb033dae13aa2186070d710c3b0c6802c52e
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-23 00:54:59 -0500

    add style to error boundary

--- app/interface/src/components/ErrorBoundaries/RootErrorBoundary/index.tsx
+++ app/interface/src/components/ErrorBoundaries/RootErrorBoundary/index.tsx
@@ -27,11 +27,15 @@ const RootErrorBoundary: React.FC<RootErrorBoundaryProps> = ({ children }) => {
 
   if (hasError) {
     return (
-      <div>
-        <h2>Something went wrong</h2>
-        <p>Sorry, something went wrong. Please try again.</p>
-        <Button variant="text" onClick={handleRefresh}>
-          Try Again
+      <div className="flex h-screen w-screen flex-col items-center justify-center gap-8">
+        <h1 className="text-5xl">😧</h1>
+        <h2 className="text-3xl">Oh snap! Something went wrong!</h2>
+        <p>
+          Sorry, something went wrong. If this keeps happening over and over
+          again, please contact us.
+        </p>
+        <Button variant="primary" onClick={handleRefresh}>
+          Lets start all over again!
         </Button>
       </div>
     );
