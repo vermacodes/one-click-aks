2db55a19599760f5a30faedce773cb2dff92100b
vermacodes <19263858+vermacodes@users.noreply.github.com>
2023-11-26 21:03:28 -0500

    use global state

--- app/interface/src/components/Builder/KubernetesCluster/Addons/HttpApplicationRouting/index.tsx
+++ app/interface/src/components/Builder/KubernetesCluster/Addons/HttpApplicationRouting/index.tsx
@@ -1,44 +1,39 @@
 import { useContext } from "react";
-import { useLab, useSetLab } from "../../../../../hooks/useLab";
-import Checkbox from "../../../../UserInterfaceComponents/Checkbox";
-import { WebSocketContext } from "../../../../Context/WebSocketContext";
 import { useSetLogs } from "../../../../../hooks/useLogs";
+import { useGlobalStateContext } from "../../../../Context/GlobalStateContext";
+import { WebSocketContext } from "../../../../Context/WebSocketContext";
+import Checkbox from "../../../../UserInterfaceComponents/Checkbox";
 
 type Props = { index: number };
 
 export default function HttpApplicationRouting({ index }: Props) {
   const { actionStatus } = useContext(WebSocketContext);
   const { mutate: setLogs } = useSetLogs();
-  const {
-    data: lab,
-    isLoading: labIsLoading,
-    isFetching: labIsFetching,
-  } = useLab();
+  const { lab, setLab } = useGlobalStateContext();
 
-  const { mutate: setLab } = useSetLab();
-
-  const cluster = lab?.template?.kubernetesClusters[index];
+  const newLab = { ...lab };
+  const cluster = newLab?.template?.kubernetesClusters[index];
 
   // Handle checkbox change
   const handleOnChange = () => {
     // If the cluster and its addons exist
-    if (cluster?.addons && lab !== undefined) {
+    if (cluster?.addons && newLab !== undefined) {
       // Toggle the httpApplicationRouting addon
       cluster.addons.httpApplicationRouting =
         !cluster.addons.httpApplicationRouting;
 
       // If there's no action in progress, set the logs with the updated lab template
       !actionStatus.inProgress &&
-        setLogs({ logs: JSON.stringify(lab?.template, null, 4) });
+        setLogs({ logs: JSON.stringify(newLab?.template, null, 4) });
 
       // Update the lab data with the updated cluster
-      setLab(lab);
+      setLab(newLab);
     }
   };
 
   // Determine checked and disabled states
   const checked = cluster?.addons?.httpApplicationRouting ?? false;
-  const disabled = labIsLoading || labIsFetching || !cluster;
+  const disabled = !cluster;
 
   // Render the Checkbox component if the lab template exists
   return lab?.template ? (
